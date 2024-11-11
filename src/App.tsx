818060a7b77f6406fe005248121eca479dbdcf00
DenzelRdz <danielrdz.ddro@gmail.com>
2024-11-11 12:28:07 -0600

    Proyecto Terminado

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+import CriptoSearchForm from "./components/CriptoSearchForm"
+import { useCryptoStore } from "./store"
+import CryptoPriceDisplay from "./components/CryptoPriceDisplay"
+import './Spinner.css'
+
+function App() {
+  // const fetchCryptos = useCryptoStore((state) => state.fetchCryptos)
+  const { fetchCryptos } = useCryptoStore()
+  
+  useEffect(()=> {
+    fetchCryptos()
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+
+        <div className="content">
+          <CriptoSearchForm/>
+          <CryptoPriceDisplay/>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
